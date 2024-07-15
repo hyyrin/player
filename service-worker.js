8bee63ba866868ca42bf2380f4c1110d240aa5f1
hyyrin <chen664181041@gmail.com>
2024-07-16 05:16:33 +0800

    new

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+// service-worker.js
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // 可以在这里处理离线缓存等
+});
